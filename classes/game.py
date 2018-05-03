96646842761eac6c1356aa3eb602a71990465008
cbogues <YOUR-EMAIL-ADDRESS>
2018-05-03 06:05:31 -0700

    Starting over but 'print' is not working for printing the attack

--- classes/game.py
+++ classes/game.py
@@ -29,7 +29,7 @@ class Person:
         mgl = self.magic[i]["dmg"] - 5
         mgh = self.magic[i]["dmg"] + 5
         return random.randrange(mgl, mgh)
-
+    
     def take_damage(self, dmg):
         self.hp -= dmg
         if self.hp < 0:
@@ -41,17 +41,17 @@ class Person:
     
     def get_max_hp(self):
         return self.maxhp
-
+    
     def get_mp(self):
         return self.mp
     
     def get_max_mp(self):
         return self.maxmp
-    
+
     def reduce_mp(self, cost):
         self.mp -= cost
     
-    def get_spell_name(self, i): 
+    def get_spell_name(self, i):
         return self.magic[i]["name"]
     
     def get_spell_mp_cost(self, i):
@@ -61,13 +61,11 @@ class Person:
         i = 1
         print("Actions")
         for item in self.actions:
-            print(str(i) + "i", item)
-            i += 1
+            print(str(i) + ":", item)
+            i += 1 
     
     def choose_magic(self):
         i = 1
-        print("Actions")
+        print("Magic")
         for spell in self.magic:
-            print(str(i) + "i", spell["name"], "(cost:", str(spell["mp"]) + ")")
-            i += 1
-
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
